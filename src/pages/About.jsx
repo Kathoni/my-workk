54b126257d9edce694b8e995ebb07930b90e70ea
dalienst <oduordalienst@gmail.com>
2024-06-06 08:40:29 +0300

    edit about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,56 +1,42 @@
+import React from "react";
+import Myimage from "../assets/images/banner.jpeg";
+import Ours from "../assets/images/ourabout.jpeg";
 
-import Myimage from '../assets/images/banner.jpeg'
-import Ours from '../assets/images/ourabout.jpeg'
 function About() {
   return (
-    <div>
-      <div className='myabout'><img src={Ours}  /><br /></div>
-    <div className='about'>
-    <div className='container'>
-      <div className='card'>
-        <div className='card-shadow'>
-    <h1>About us</h1> 
-    <h2>We are a world operating company ;</h2>
-    <p>We offer services based on any geographical <br />
-    area you may be in.</p>
-    <h1>You request and we deliver.</h1>
-    <p>On our long journey we have been able to meet several 
-      different people with different tastes<br/>but all in all
-      we have been able to satisfy them with our unique and <br/>
-      designs.
-    </p>
-    </div>
-    </div>
-  
-    </div>
-    <div className='container'>
-      <div className='card'>
-        <div className='card-shadow'>
-    <h1>About our services</h1> 
-    <h3>We offer a wide variety of serviced it just depends on what you want.</h3>
-    <h4> Here are some of the things we offer;</h4>
-    <div className='List'>
-      <ul>
-        <li>Wallpaper installation.</li>
-        <li>Kitchen makeover</li>
-        <li>Bedroom design</li>
-        <li>Living room design</li>
-        <li>Office design</li>
-        <li>Studio appartment design</li>
-      </ul>
-      <h4>Our list is endless but we assure you we meet our clients expectations.</h4>
-    </div>
-    </div>
-    </div>
-    </div>
-    <div>
-    <img src={Myimage}/>
-    </div>
-   
-    
-    </div>
-    </div>
-  )
+    <>
+      <div
+        className="container-fluid mb-3 d-flex flex-column align-items-center justify-content-center text-center mx-auto"
+        style={{ height: "50vh", backgroundColor: "#f4eae8" }}
+      >
+        <div className="text-center">
+          <h3 className=" fs-1">As we evolve, our homes should, too</h3>
+        </div>
+      </div>
+
+      <section className="container py-5 mb-5">
+        <div className="row">
+          <div className="col-md-6 col-sm-12 mb-3">
+            <h4 className="text-decoration-underline">About Us</h4>
+            <p className="fst-italic">
+              We are a world operating company. You request and we deliver.
+            </p>
+            <p></p>
+            <p>
+              We offer services based on any geographical area you may be in.{" "}
+              <br /> On our long journey we have been able to meet several
+              different people with different tastes but all in all we have been
+              able to satisfy them with our unique and designs.
+            </p>
+          </div>
+
+          <div className="col-md-6 col-sm-12 mb-3">
+            <img src={Myimage} alt="" className="img-fluid" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default About
+export default About;
